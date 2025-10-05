5e4578d02bf8d40852e223f2be53309aaa86302d
Fabricio Ferreira <fabricio.ferreira@outlook.com>
2025-10-05 16:30:41 +1300

    feat: extra formatters

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,26 +1,33 @@
 local ok, conform = pcall(require, "conform")
 
 if not ok then
-  return
+	return
 end
 
 conform.setup({
-  formatters_by_ft = {
-    cs = { "csharpier" }
-  },
-  format_on_save = {
-    lsp_fallback = true,
-    timeout_ms = 2000
-  },
-  formatters = {
-    csharpier = {
-      command = "dotnet-csharpier",
-      args = { "--write-stdout" },
-      stdin = true
-    },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		cs = { "csharpier" },
+		rust = { "rustfmt", lsp_format = "fallback" },
+		go = { "goimports", "gofmt" },
+		svelte = { "prettier-plugin-svelte" },
+		css = { "prettier" },
+		html = { "prettier" },
+		typescript = { "prettier" },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		timeout_ms = 2000,
+	},
+	formatters = {
+		csharpier = {
+			command = "dotnet-csharpier",
+			args = { "--write-stdout" },
+			stdin = true,
+		},
+	},
 })
 
-vim.keymap.set({"", "n", "v" }, "<leader>f", function()
-  conform.format({ async = true, lsp_fallback = true })
-end, {desc = "Format buffer"})
+vim.keymap.set({ "", "n", "v" }, "<leader>f", function()
+	conform.format({ async = true, lsp_fallback = true })
+end, { desc = "Format buffer" })
--- lua/fabricio/packer.lua
+++ lua/fabricio/packer.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -18,51 +18,51 @@ return require('packer').startup(function(use)
 
   use('BurntSushi/ripgrep')
   use {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      run = 'make'
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make'
   }
   use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   use({
-	  'christianchiarulli/nvcode-color-schemes.vim',
-	  as = 'nvcode',
-	  config = function()
-		  vim.cmd('colorscheme nvcode')
-	  end
+    'christianchiarulli/nvcode-color-schemes.vim',
+    as = 'nvcode',
+    config = function()
+      vim.cmd('colorscheme nvcode')
+    end
   })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/playground')
 
-  use ({
-	  "ThePrimeagen/harpoon",
-	  branch = "harpoon2",
-	  requires = { {"nvim-lua/plenary.nvim"} }
+  use({
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { { "nvim-lua/plenary.nvim" } }
   })
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
-  use('fatih/vim-go', {run=':GoUpdateBinaries'})
+  use('fatih/vim-go', { run = ':GoUpdateBinaries' })
 
   -- LSP Support
-  use({'neovim/nvim-lspconfig'})             -- Required
-  use({'williamboman/mason.nvim'})           -- Optional
-  use({'williamboman/mason-lspconfig.nvim'}) -- Optional
+  use({ 'neovim/nvim-lspconfig' })             -- Required
+  use({ 'williamboman/mason.nvim' })           -- Optional
+  use({ 'williamboman/mason-lspconfig.nvim' }) -- Optional
 
   -- Autocompletion
-  use({'hrsh7th/nvim-cmp'})         -- Required
-  use({'hrsh7th/cmp-nvim-lsp'})     -- Required
-  use({'hrsh7th/cmp-buffer'})       -- Optional
-  use({'hrsh7th/cmp-path'})         -- Optional
-  use({'saadparwaiz1/cmp_luasnip'}) -- Optional
-  use({'hrsh7th/cmp-nvim-lua'})     -- Optional
+  use({ 'hrsh7th/nvim-cmp' })         -- Required
+  use({ 'hrsh7th/cmp-nvim-lsp' })     -- Required
+  use({ 'hrsh7th/cmp-buffer' })       -- Optional
+  use({ 'hrsh7th/cmp-path' })         -- Optional
+  use({ 'saadparwaiz1/cmp_luasnip' }) -- Optional
+  use({ 'hrsh7th/cmp-nvim-lua' })     -- Optional
 
   -- Snippets
-  use({'L3MON4D3/LuaSnip'})             -- Required
-  use({'rafamadriz/friendly-snippets'}) -- Optional
+  use({ 'L3MON4D3/LuaSnip' })             -- Required
+  use({ 'rafamadriz/friendly-snippets' }) -- Optional
 
   -- essential plugins
   use("tpope/vim-surround")
@@ -79,8 +79,8 @@ return require('packer').startup(function(use)
 
   -- lualine
   use {
-      "nvim-lualine/lualine.nvim",
-      requires = { "nvim-tree/nvim-web-devicons", opt = true }
+    "nvim-lualine/lualine.nvim",
+    requires = { "nvim-tree/nvim-web-devicons", opt = true }
   }
 
   use("jlcrochet/vim-cs")
@@ -88,14 +88,21 @@ return require('packer').startup(function(use)
   -- Formatting
   use {
     "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup()
+    end,
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
     requires = "nvim-lspconfig"
+  }
+
+  use { "zapling/mason-conform.nvim",
+    config = function()
+      require("mason-conform").setup({ ignore_install = { 'prettier' } })
+    end
   }
 
   if packer_bootstrap then
     require("packer").sync()
   end
 end)
-
-
